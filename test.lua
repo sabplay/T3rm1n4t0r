d9543461c51d43c1249205cbea46a041a92328d5
sabplay <87631627+sabplay@users.noreply.github.com>
2023-09-05 18:26:32 +0530

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,75 +1,65 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("T3rmin4t0r H4x", "DarkTheme")
 
-if game.PlaceId == 286090429 then
--- [ MAIN ] --
 local M4in = Window:NewTab("M4in")
-local Sh00t3r = M4in:NewSection("4rs3n4l")
+local T3ST = M4in:NewSection("T3ST")
 
-Sh00t3r:NewButton("4iml0ck+4imb0t", "s4fe", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/arsenal.lua'))()
+T3ST:NewButton("T3ST", "T3ST", function()
+    print("T3ST3ED SUCC3SSFULLY!")
     game.StarterGui:SetCore("SendNotification", {
-        Title = "G0 B4NG TH31R M0TH3RS!"; -- the title (ofc)
+        Title = "T3ST3ED SUCC3SSFULLY!"; -- the title (ofc)
         Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
         Icon = "rbxassetid://14682870738"; -- the image if u want. 
         Duration = 5; -- how long the notification should in secounds
-         })
- end)
-end
-if game.PlaceId == 12562525439 then
-    local M4in = Window:NewTab("M4in")
-    local T3ST = M4in:NewSection("T3ST")
-    
-    T3ST:NewButton("T3ST", "T3ST", function()
-        print("T3ST3ED SUCC3SSFULLY!")
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "T3ST3ED SUCC3SSFULLY!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
-        end)
-    T3ST:NewButton("Fly T3ST", "fly", function()
-        loadstring(game:HttpGet('https://pastebin.com/raw/2J4GfniA'))()
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "FLY!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
+        })
     end)
-    T3ST:NewButton("D4RK D3X", "D4RK D3X", function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua'))()
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "CH4NG3 TH3 G4ME!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
-    end)
-end
+T3ST:NewButton("Fly T3ST", "fly", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/2J4GfniA'))()
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "FLY!"; -- the title (ofc)
+        Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
+        Icon = "rbxassetid://14682870738"; -- the image if u want. 
+        Duration = 5; -- how long the notification should in secounds
+        })
+end)
 
-if game.PlaceId == 12334109280 then
-    local M4in = Window:NewTab("M4in")
-    local GUT5 = M4in:NewSection("GUT5 4ND BL4CK P0WD3R")
-
-    GUT5:NewButton("3SP", "3SP", function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/g%26bp.lua'))()
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "S33 TH3M!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
-        end)
-
-        GUT5:NewButton("Fly T3ST", "fly", function()
-            loadstring(game:HttpGet('https://pastebin.com/raw/2J4GfniA'))()
-            game.StarterGui:SetCore("SendNotification", {
-            Title = "FLY!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-               })
-    end)
-end
+T3ST:NewKeybind("UI (Y)", "makes the gui invisible, press again to make it visible", Enum.KeyCode.Y, function()
+	Library:ToggleUI()
+end)
+T3ST:NewSlider("L0CAL 3D1T0R T3ST", "", function() -- 500 (MaxValue) | 0 (MinValue)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "CH4NG3 TH3 G4ME!"; -- the title (ofc)
+        Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
+        Icon = "rbxassetid://14682870738"; -- the image if u want. 
+        Duration = 5; -- how long the notification should in secounds
+        })
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("T3rmin4t0r H4x SP33D", "Midnight")
+--[[
+    LightTheme
+    DarkTheme
+    GrapeTheme
+    BloodTheme
+    Ocean
+    Midnight
+    Sentinel
+    Synapse
+]]
+local Tab = Window:NewTab("LocalPlayer")
+local Section = Tab:NewSection("SP33D")
+Section:NewTextBox("Character speed", "Type in a number to make the speed u want", function(txt)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = txt
+end)
+Section:NewSlider("SP33D SL1D3R", "Yo", 500, 0, function(speed)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
+end)
+Section:NewButton("Infinite yield", "alot of better", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+Section:NewKeybind("UI (U)", "makes the gui invisible, press again to make it visible", Enum.KeyCode.U, function()
+	Library:ToggleUI()
+end)
+T3ST:NewButton("D4RK D3X", "D4RK D3X", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua'))()
+end)
+end)
